4fd8c9aeb638f78524ce220f3f82ed7be378ac28
Wataru KUNINO <git@bokunimo.net>
2023-03-29 20:16:58 +0900

    WiFi

--- examples/example03_wifi.py
+++ examples/example03_wifi.py
@@ -5,57 +5,55 @@
 # Error ENOMEM や EADDRINUSE が出た場合はハードウェアリセットを実行してください
 #   machine.reset()
 
-SSID = "12345678"
-PASS = "password"
-udp_to = '255.255.255.255'                              # UDPブロードキャスト
-udp_port = 1024                                         # UDPポート番号
-device_s = 'temp._3'                                    # デバイス識別名
-interval = 10                                           # 送信間隔（秒）
-temp_offset = 8.0                                       # CPU温度上昇値(要調整)
+SSID = "1234ABCD"                               # 無線LANアクセスポイント SSID
+PASS = "password"                               # パスワード
 
-from machine import ADC,Pin             # ライブラリmachineのADCを組み込む
-from machine import UART                # machineからUARTを組み込む
-from utime import sleep                 # μtimeからsleepを組み込む
-import network                                          # ネットワーク通信
-import usocket                                          # μソケット通信
+udp_to = '255.255.255.255'                      # UDPブロードキャスト
+udp_port = 1024                                 # UDPポート番号
+device_s = 'temp._3'                            # デバイス識別名
+interval = 10                                   # 送信間隔（秒）
+temp_offset = 8.0                               # CPU温度上昇値(要調整)
 
-led = Pin(25, Pin.OUT)                  # GPIO出力用インスタンスledを生成
-adc = ADC(4)                            # 温度センサ用ADCポートadcを生成
+from machine import ADC,Pin                     # machineからADCを組み込む
+from machine import UART                        # machineからUARTを組み込む
+from utime import sleep                         # μtimeからsleepを組み込む
+import network                                  # ネットワーク通信
+import usocket                                  # μソケット通信
 
-eth = network.Ethernet()                                # Ethernet用のethを生成
-try:                                                    # 例外処理の監視を開始
-    eth.active(True)                                    # Ethernetを起動
-    eth.ifconfig('dhcp')                                # DHCPクライアントを設定
-except Exception as e:                                  # 例外処理発生時
-    while True:
-        print(e)                                        # エラー内容を表示
-        sleep(3)                                        # 3秒の待ち時間処理
-eth.active(True)
-eth.connect(SSID, PASS)
+led = Pin(25, Pin.OUT)                          # GPIO出力用ledを生成
+adc = ADC(4)                                    # 温度センサ用adcを生成
 
-while True:                             # 繰り返し処理
-    val = adc.read_u16()                # ADCから値を取得して変数valに代入
-    mv = val * 3300 / 65535             # ADC値を電圧(mV)に変換
-    temp = 27 - (mv - 706) / 1.721      # ADC電圧値を温度(℃)に変換
-    s = str(round(temp,1))              # 小数点第1位で丸めた結果を文字列に
-    print('Temperature =',s)            # 温度値を表示
-    s = ble_ad_id + '{:04X}'.format(val)# BLE送信データの生成(16進数に変換)
-    led.value(1)                        # LEDをONにする
+wlan = network.WLAN(network.STA_IF)             # Ethernet用のethを生成
+wlan.active(True)                               # Ethernetを起動
+wlan.connect(SSID, PASS)
+while True:
+    if wlan.status() == 3:
+        break
+    print('.', end='')
+    sleep(1)
+ifconf = wlan.ifconfig()
+print('\n',ifconf)
+
+while True:                                     # 繰り返し処理
+    val = adc.read_u16()                        # ADC値を取得して変数valに代入
+    mv = val * 3300 / 65535                     # ADC値を電圧(mV)に変換
+    temp = 27 - (mv - 706) / 1.721              # ADC電圧値を温度(℃)に変換
+    temp -= temp_offset                         # temp_offsetを減算
+    temp_i = round(temp)                        # 整数に変換してtemp_iへ
+    print('Temperature =',temp_i,'('+str(temp)+')') # 温度値を表示する
+    led.value(1)                                # LEDをONにする
 
     sock = usocket.socket(usocket.AF_INET,usocket.SOCK_DGRAM) # μソケット作成
     #      ~~~~~~~        ~~~~~~~         ~~~~~~~
-    udp_s = device_s + ', ' + str(temp_i)               # 表示用の文字列変数udp
-    print('send :', udp_s)                              # 受信データを出力
-    udp_bytes = (udp_s + '\n').encode()                 # バイト列に変換
+    udp_s = device_s + ', ' + str(temp_i)       # 表示用の文字列変数udp
+    print('send :', udp_s)                      # 受信データを出力
+    udp_bytes = (udp_s + '\n').encode()         # バイト列に変換
 
     try:
-        sock.sendto(udp_bytes,(udp_to,udp_port))        # UDPブロードキャスト送信
+        sock.sendto(udp_bytes,(udp_to,udp_port)) # UDPブロードキャスト送信
+    except Exception as e:                      # 例外処理発生時
+        print(e)                                # エラー内容を表示
+    sock.close()                                # ソケットの切断
 
-    except Exception as e:                              # 例外処理発生時
-        print(e)                                        # エラー内容を表示
-
-    sock.close()                                        # ソケットの切断
-
-
-    led.value(0)                        # LEDをOFFにする
-    sleep(5)                            # 5秒間の待ち時間処理
+    led.value(0)                                # LEDをOFFにする
+    sleep(5)                                    # 5秒間の待ち時間処理
